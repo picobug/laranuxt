253015222ce4c3b3d05a674a4cd16a848b6789c8
Julian Prasetyo <jul14n4v@gmail.com>
2018-04-06 09:54:57 +0700

    Initial Release

---
+++ resources/nuxt/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Hello {{ name }}!</h1>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return { name: "world" }
+  },
+  head() {
+    return {
+      title: "About Us - Nuxt.js",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "About our company Nuxt.js "
+        }
+      ]
+    }
+  }
+}
+</script>
